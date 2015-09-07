0037146c172703aa3443bde136d0b57c639d091d
jumo <ju.jumo@gmail.com>
2015-09-07 21:33:31 +0200

    -split file / raw data, add encrypt, no more page related actions

--- js/ssl_decrypt.js
+++ js/ssl_decrypt.js
@@ -1,28 +1,35 @@
-
-function decrypt(){
-	name = $( "#loggin").val();
-	passwd = $( "#password").val();
-	// Load encrypted data file
-	var stringData = $.ajax({
-					url: name + ".html.cyph.txt",
-					async: false
-				 }).responseText;
+function decrypt(stringData, passwd){
 	// remove '\n' from message
 	stringData = stringData.replace(/\r?\n|\r/g, "");
 	// decrypt message
 	var de = CryptoJS.AES.decrypt(stringData, passwd, {keySize: 256/32});
 	// decode content into UTF-8
 	var content = (de.toString(CryptoJS.enc.Utf8));
-	// display content
-	var newDoc = document.open("text/html", "replace");
-	newDoc.write(content);
-	newDoc.close();
+	return content;
 }
 
-$(function(){
-    $(this).find('input').keypress(function(e){
-        if(e.which == 10 || e.which == 13) {
-            decrypt();
-        }
-    });
-});
+function decrypt_file(filename, passwd){
+	var stringData = $.ajax({
+					url: filename + ".html.cyph.txt",
+					async: false
+				 }).responseText;
+	// decode content into UTF-8
+	var content = decrypt(stringData, passwd);
+	return content;
+}
+
+function encrypt(stringData, passwd){
+	// decrypt message
+	var en = CryptoJS.AES.encrypt(stringData, passwd, {keySize: 256/32});
+	var content = (en.toString());
+	return content;
+}
+
+function encrypt_file(filename, passwd){
+	var stringData = $.ajax({
+					url: filename + ".html.cyph.txt",
+					async: false
+				 }).responseText;
+	var content = encrypt(stringData, passwd);
+	return content;
+}
