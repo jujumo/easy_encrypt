1e8883b168be63a34490c2512aa0826336b53fad
jumo <ju.jumo@gmail.com>
2015-09-12 09:34:43 +0200

    split file / crypt

--- js/file_exchange.js
+++ js/file_exchange.js
@@ -1,19 +1,3 @@
-function decrypt(stringData, passwd){
-	// remove '\n' from message
-	stringData = stringData.replace(/\r?\n|\r/g, "");
-	// decrypt message
-	var de = CryptoJS.AES.decrypt(stringData, passwd, {keySize: 256/32});
-	// decode content into UTF-8
-	var content = (de.toString(CryptoJS.enc.Utf8));
-	return content;
-}
-
-function encrypt(content, passwd){
-	var en = CryptoJS.AES.encrypt(content, passwd, {keySize: 256/32});
-	content = (en.toString());
-	return content;
-}
-
 function read_file_from_url(filename) {
 	var content = $.ajax({
 					url: filename,
@@ -54,16 +38,4 @@ function write_file_to_user(filename, content) {
 	document.body.appendChild(element);
 	element.click();
 	document.body.removeChild(element);
-}
-
-function encrypt_file(filename, passwd){
-	var stringData = read_file_from_url(filename);
-	var content = encrypt(stringData, passwd);
-	return content;
-}
-
-function decrypt_file(filename, passwd){
-	var stringData = read_file_from_url(filename);
-	var content = decrypt(stringData, passwd);
-	return content;
 }
