ff1e4bec723a24dc8948e1e64951ae0c69eb2661
jumo <ju.jumo@gmail.com>
2018-12-05 20:03:32 +0100

    init some py tests

--- py/ssl_encrypt_test.py
+++ py/ssl_encrypt_test.py
@@ -1,14 +1,26 @@
 
 import unittest
 from ssl_encrypt import encrypt_file, decrypt_file
+import tempfile
 
 
-class MyTestCase(unittest.TestCase):
-    def test_default_greeting_set(self):
+class FileCipherTest(unittest.TestCase):
+    def test_cipher_decipher(self):
         password = 'password'
-        greeter = encrypt_file(in_file, out_file, password)
-        print('test')
-        # self.assertEqual(greeter.message, 'Hello world!')
+        content_expected = bytearray('papa', encoding='utf-8')
+        with tempfile.NamedTemporaryFile() as clear_f, tempfile.NamedTemporaryFile() as cipher_file:
+            clear_f.write(content_expected)
+            clear_f.seek(0)
+            cipher_file.seek(0)
+            greeter = encrypt_file(clear_f, cipher_file, password)
+            clear_f.seek(0)
+            cipher_file.seek(0)
+            greeter = decrypt_file(cipher_file, clear_f, password)
+            clear_f.seek(0)
+            cipher_file.seek(0)
+            content_retrieved = clear_f.read()
+            self.assertTrue\
+                (content_retrieved == content_expected)
 
 
 if __name__ == '__main__':
